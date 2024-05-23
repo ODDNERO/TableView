2ec6a30efc1bb2653eb3d31ad89ed585a35680d3
NERO <mxzxro@gmail.com>
2024-05-23 20:21:29 +0900

    [Storyboard] 집중 모드

---
+++ TableView/TableViewController.swift
@@ -0,0 +1,27 @@
+//
+//  TableViewController.swift
+//  TableView
+//
+//  Created by NERO on 5/23/24.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+    override func numberOfSections(in tableView: UITableView) -> Int {
+
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        return 0
+    }
+}
