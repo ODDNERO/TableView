e9300792532865e5c831f8d32414ffc7bb7d1b9f
NERO <mxzxro@gmail.com>
2024-06-04 00:00:15 +0900

    ♻️ < 설정 화면 > enum 활용 Code Refactoring

--- TableView/SettingTableViewController.swift
+++ TableView/SettingTableViewController.swift
@@ -7,9 +7,33 @@
 
 import UIKit
 
+enum Setting: Int, CaseIterable {
+    case total, personal, others
+    
+    var category: String {
+        switch self {
+        case .total:
+            return "전체 설정"
+        case .personal:
+            return "개인 설정"
+        case .others:
+            return "기타"
+        }
+    }
+    
+    var options: [String] {
+        switch self {
+        case .total:
+            return ["공지사항", "실험실", "버전 정보"]
+        case .personal:
+            return ["개인/보안", "알림", "채팅", "멀티프로필"]
+        case .others:
+            return ["고객센터/도움말"]
+        }
+    }
+}
+
 class SettingTableViewController: UITableViewController {
-    let sectionTitles = ["전체 설정", "개인 설정", "기타"]
-    let labelTexts = [["공지사항", "실험실", "버전 정보"], ["개인/보안", "알림", "채팅", "멀티프로필"], ["고객센터/도움말"]]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,23 +44,24 @@ class SettingTableViewController: UITableViewController {
 
     // MARK: - Table view data source
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return sectionTitles.count
+        return Setting.allCases.count
     }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return sectionTitles[section]
+        return Setting.allCases[section].category
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return labelTexts[section].count
+        return Setting.allCases[section].options.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "settingCell")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "settingCell", for: indexPath)
+        let section = Setting.allCases[indexPath.section]
         
-        cell?.textLabel?.text = labelTexts[indexPath.section][indexPath.row]
-        cell?.textLabel?.font = .systemFont(ofSize: 13)
+        cell.textLabel?.text = section.options[indexPath.row]
+        cell.textLabel?.font = .systemFont(ofSize: 13)
         
-        return cell!
+        return cell
     }
 }
