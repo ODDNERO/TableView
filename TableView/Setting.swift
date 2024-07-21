4fc6f1c85522fc5b3a9ce5df2e64e87999802601
NERO <mxzxro@gmail.com>
2024-07-21 23:06:33 +0900

    🩹 < 설정 화면 > Setting enum 파일 분리

---
+++ TableView/Setting.swift
@@ -0,0 +1,34 @@
+//
+//  Setting.swift
+//  TableView
+//
+//  Created by NERO on 7/20/24.
+//
+
+import Foundation
+
+enum Setting: Int, CaseIterable {
+    case total, personal, others
+    
+    var category: String {
+        switch self {
+        case .total:
+            return "전체 설정"
+        case .personal:
+            return "개인 설정"
+        case .others:
+            return "기타"
+        }
+    }
+    
+    var options: [String] {
+        switch self {
+        case .total:
+            return ["공지사항", "실험실", "버전 정보"]
+        case .personal:
+            return ["개인/보안", "알림", "채팅", "멀티프로필"]
+        case .others:
+            return ["고객센터/도움말"]
+        }
+    }
+}
